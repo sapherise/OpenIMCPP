c081f7bfbe1638a7dcbd77cb3e3c0a2710cb16e8
Sa <sa@heijing.space>
2025-09-10 00:38:15 +0800

    update mac.sh

--- gen_mac_dylib.sh
+++ gen_mac_dylib.sh
@@ -6,4 +6,6 @@ go build -buildmode=c-shared -o ./mac/libopenimsdk_arm64.dylib ./
 
 export GOARCH=amd64
 go build -buildmode=c-shared -o ./mac/libopenimsdk_amd64.dylib ./
-lipo -create -output libopenimsdk.dylib libopenimsdk_arm64.dylib libopenimsdk_amd64.dylib
+lipo -create -output ./mac/libopenimsdk.dylib \
+  ./mac/libopenimsdk_arm64.dylib \
+  ./mac/libopenimsdk_amd64.dylib
